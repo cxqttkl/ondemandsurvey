a3db0f0fc13879aedae8613df37a134419e8f2e6
cxqttkl <carto@hubu.edu.cn>
2018-05-17 20:20:11 +0800

    增加了百度地图查询
    增加了百度地图查询功能

--- js/q.js
+++ js/q.js
@@ -1,12 +1,35 @@
-var qwords="";
-var qString="https://nominatim.openstreetmap.org/search?q="+qwords+"&format=json&viewbox=118,27,123,32&countrycodes=cn&bounded=1";
+var qwords = "";
+var qString = "https://nominatim.openstreetmap.org/search?q=" + qwords + "&format=json&viewbox=118,27,123,32&countrycodes=cn&bounded=1";
+var localBaidu = null;
 
+function queryOSM() {
 
-function queryOSM(){
-   
-    var input_key =$("#keyWords");
+    var input_key = $("#keyWords");
     qwords = $("#keyWords").val();
-    qString="https://nominatim.openstreetmap.org/search?q="+qwords+"&format=json&viewbox=118,27,123,32&countrycodes=cn&bounded=1";
-  
-	geocoder();
+    //qString="https://nominatim.openstreetmap.org/search?q="+qwords+"&format=json&viewbox=118,27,123,32&countrycodes=cn&bounded=1";
+
+    //geocoder();
+    searchBaidu();
+}
+
+function searchBaidu() {
+    if (Satellitemap) {
+        qwords = $("#keyWords").val();
+        if (qwords.length > 0) {
+            if (localBaidu) {
+                localBaidu.search(qwords);
+            } else {
+                //初始化Search的过程
+                localBaidu = new BMap.LocalSearch(Satellitemap, {
+                    renderOptions: {
+                        map: Satellitemap
+                    }
+                });
+                localBaidu.search(qwords);
+            }
+
+
+        }
+    }
+
 }
